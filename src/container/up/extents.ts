a39a5e4d4f0ec1b24c7039e2388ee1cd30659654
Brad Sickles <brad_sickles@mckinsey.com>
2016-07-13 09:01:18 -0400

    Fixing transform of extents by transform in container

--- src/container/up/extents.ts
+++ src/container/up/extents.ts
@@ -19,7 +19,7 @@ namespace puck.container.up.extents {
         for (var ccomps = bag.ccomposites, i = 0; i < ccomps.length; i++) {
             rect.union(comp.extents, ccomps[i].extents);
         }
-        rect.transform(comp.extents, comp.transform);
+        rect.transform(comp.extents, comp.transform, comp.extents);
 
         if (rect.equal(comp.extents, oldExtents))
             return false;
