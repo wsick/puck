024ba55946133efb5d65afecf1651ffd26d87a25
Brad Sickles <brad_sickles@mckinsey.com>
2016-09-05 11:39:55 -0400

    Migrating Polyline properties.

--- src/Polyline.ts
+++ src/Polyline.ts
@@ -30,124 +30,149 @@ namespace puck {
             });
         }
 
-        get points() {
+        points(): Points {
             return this.state.points;
         }
 
-        get closed() {
-            return this.closed;
-        }
-
-        set closed(value: boolean) {
+        closed(): boolean;
+        closed(value: boolean): this;
+        closed(value?: boolean): any {
+            if (arguments.length < 1)
+                return this.state.closed;
             if (this.state.closed !== value) {
                 this.state.closed = value;
                 this.composite.invalidate();
             }
+            return this;
         }
 
-        get x(): number {
-            return this.state.offset.x;
-        }
-
-        set x(value: number) {
+        x(): number;
+        x(value: number): this;
+        x(value?: number): any {
+            if (arguments.length < 1)
+                return this.state.offset.x;
             if (this.state.offset.x !== value) {
                 this.state.offset.x = value;
                 this.composite.taint(DirtyFlags.transform);
             }
+            return this;
         }
 
-        get y(): number {
-            return this.state.offset.y;
-        }
-
-        set y(value: number) {
+        y(): number;
+        y(value: number): this;
+        y(value?: number): any {
+            if (arguments.length < 1)
+                return this.state.offset.y;
             if (this.state.offset.y !== value) {
                 this.state.offset.y = value;
                 this.composite.taint(DirtyFlags.transform);
             }
+            return this;
         }
 
-        get width(): number {
-            return this.state.size.width;
-        }
-
-        set width(value: number) {
+        width(): number;
+        width(value: number): this;
+        width(value?: number): any {
+            if (arguments.length < 1)
+                return this.state.size.width;
             if (this.state.size.width !== value) {
                 this.state.size.width = value;
                 this.composite.taint(DirtyFlags.stretch | DirtyFlags.transform);
             }
+            return this;
         }
 
-        get height(): number {
-            return this.state.size.height;
-        }
-
-        set height(value: number) {
+        height(): number;
+        height(value: number): this;
+        height(value?: number): any {
+            if (arguments.length < 1)
+                return this.state.size.height;
             if (this.state.size.height !== value) {
                 this.state.size.height = value;
                 this.composite.taint(DirtyFlags.stretch | DirtyFlags.transform);
             }
+            return this;
         }
 
-        get stretch(): Stretch {
-            return this.state.stretch;
-        }
-
-        set stretch(value: Stretch) {
+        stretch(): Stretch;
+        stretch(value: Stretch): this;
+        stretch(value?: Stretch): any {
+            if (arguments.length < 1)
+                return this.state.stretch;
             if (this.state.stretch !== value) {
                 this.state.stretch = value;
                 this.composite.taint(DirtyFlags.stretch);
             }
+            return this;
         }
 
-        get fillRule() {
-            return this.state.fillRule;
+        path(): curve.Path;
+        path(value: curve.Path): this;
+        path(value?: curve.Path): any {
+            if (arguments.length < 1)
+                return this.state.path;
+            if (this.state.path !== value) {
+                this.state.path = value;
+                this.composite.bounder.setPath(value);
+                this.composite
+                    .taint(DirtyFlags.padding)
+                    .invalidate();
+            }
+            return this;
         }
 
-        set fillRule(value: FillRule) {
+        fillRule(): FillRule;
+        fillRule(value: FillRule): this;
+        fillRule(value?: FillRule): any {
+            if (arguments.length < 1)
+                return this.state.fillRule;
             if (this.state.fillRule !== value) {
                 this.state.fillRule = value;
                 this.composite.invalidate();
             }
+            return this;
         }
 
-        get strokeLineCap() {
-            return this.state.strokeLineCap;
-        }
-
-        set strokeLineCap(value: PenLineCap) {
+        strokeLineCap(): PenLineCap;
+        strokeLineCap(value: PenLineCap): this;
+        strokeLineCap(value?: PenLineCap): any {
+            if (arguments.length < 1)
+                return this.state.strokeLineCap;
             if (this.state.strokeLineCap !== value) {
                 this.state.strokeLineCap = value;
                 this.composite
                     .taint(DirtyFlags.padding)
                     .invalidate();
             }
+            return this;
         }
 
-        get strokeLineJoin() {
-            return this.state.strokeLineJoin;
-        }
-
-        set strokeLineJoin(value: PenLineJoin) {
+        strokeLineJoin(): PenLineJoin;
+        strokeLineJoin(value: PenLineJoin): this;
+        strokeLineJoin(value?: PenLineJoin): any {
+            if (arguments.length < 1)
+                return this.state.strokeLineJoin;
             if (this.state.strokeLineJoin !== value) {
                 this.state.strokeLineJoin = value;
                 this.composite
                     .taint(DirtyFlags.padding)
                     .invalidate();
             }
+            return this;
         }
 
-        get strokeMiterLimit() {
-            return this.state.strokeMiterLimit;
-        }
-
-        set strokeMiterLimit(value: number) {
+        strokeMiterLimit(): number;
+        strokeMiterLimit(value: number): this;
+        strokeMiterLimit(value?: number): any {
+            if (arguments.length < 1)
+                return this.state.strokeMiterLimit;
             if (this.state.strokeMiterLimit !== value) {
                 this.state.strokeMiterLimit = value;
                 this.composite
                     .taint(DirtyFlags.padding)
                     .invalidate();
             }
+            return this;
         }
     }
 }
